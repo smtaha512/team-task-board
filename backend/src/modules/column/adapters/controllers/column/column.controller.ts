281a9304855bcf92a3a95d2958dd5dc8e5c7e107
smtaha512 <s.m.taha10@gmail.com>
2025-08-23 22:22:15 +0500

    feat: add api docs for column controller

--- backend/src/modules/column/adapters/controllers/column/column.controller.ts
+++ backend/src/modules/column/adapters/controllers/column/column.controller.ts
@@ -1,9 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { FetchColumnsResponseBodyDto } from '../../../app/use-cases/dtos/fetch-columns.response.body.dto';
 import { FetchColumnsUseCase } from '../../../app/use-cases/fetch-columns.use-case';
 
-@Controller('/boards/columns')
+@ApiTags('columns')
+@Controller('columns')
 export class ColumnController {
   constructor(private readonly fetchColumnsUseCase: FetchColumnsUseCase) {}
+
+  @ApiOperation({ summary: 'List tasks' })
+  @ApiOkResponse({
+    type: FetchColumnsResponseBodyDto,
+    isArray: true,
+    description: 'Tasks listed successfully',
+  })
   @Get('')
   fetchColumns() {
     return this.fetchColumnsUseCase.execute();
